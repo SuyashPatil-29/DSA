36fec45206a9a1e6ddb86d1fbe9ef1f87204f67d
SuyashPatil-29 <patilsuyash892@gmail.com>
2024-05-09 22:54:11 +0530

    Sort

---
+++ 3/3.2-medium/appears-n-by-two-times.cpp
@@ -0,0 +1,23 @@
+// #include <bits/stdc++.h>
+// #include <unordered_map>
+//
+// using namespace std;
+//
+// int main() {
+//   int arr[] = {2, 2, 3, 3, 1, 2, 2};
+//   int size = sizeof(arr) / sizeof(arr[0]);
+//   unordered_map<int, int> mpp;
+//
+//   for (int i = 0; i < size; i++) {
+//     mpp[arr[i]]++;
+//   }
+//   for (auto it = mpp.begin(); it != mpp.end(); it++) {
+//     if (it->second > size / 2) {
+//       cout << it->first;
+//       return 0;
+//     }
+//   }
+//
+//   return 0;
+// }
+
